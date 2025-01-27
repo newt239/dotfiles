b9d3f4f9ee41d4cbb694c98c2f4f13275b4989c9
newt <ysti2019.win2020.st@gmail.com>
2025-01-27 13:06:37 +0900

    update: brew apps

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -7,3 +7,5 @@ fi
 
 BREWFILE_DIR="$(cd "$(dirname "$0")" && pwd)"
 brew bundle --file="$BREWFILE_DIR/Brewfile" --global
+
+mas install 1429033973 # https://github.com/Kyome22/menubar_runcat/issues/1#issuecomment-778353093
