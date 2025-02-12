05b21482057eb2563a972c5f705db5343a68c3f4
newt <ysti2019.win2020.st@gmail.com>
2025-02-12 13:01:28 +0900

    defaults.shに設定を追加

--- .bin/defaults.sh
+++ .bin/defaults.sh
@@ -5,60 +5,59 @@ if [ "$(uname)" != "Darwin" ] ; then
 	exit 1
 fi
 
-# Show bluetooth in the menu bar
-defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool true
-
-# Automatically hide or show the Dock
+echo "Dockを自動的に隠す"
 defaults write com.apple.dock autohide -bool true
 
-# Avoid creating `.DS_Store` files on network volumes
+echo ".DS_Store ファイルを作成しない"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# Show the full POSIX path as Finder window title
+echo "Finder のタイトルバーにフルパスを表示"
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# Show hidden files in Finder
+echo "Finder で隠しファイルを表示"
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
-# Show path bar in Finder
+echo "Finder でファイルの拡張子を表示"
 defaults write com.apple.finder ShowPathbar -bool true
 
-# Show status bar in Finder
+echo "Finder でステータスバーを表示"
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Show Tab bar in Finder
+echo "Finder でタブバーを表示"
 defaults write com.apple.finder ShowTabView -bool true
 
-# Disable the "Are you sure you want to open this application?" dialog
-defaults write com.apple.LaunchServices LSQuarantine -bool false
+echo "時計のフォーマットを「曜日 日 月 時:分」にする"
+defaults write com.apple.menuextra.clock DateFormat -string 'd/MMM(EEE) HH:mm:ss'
 
-# Display battery level in the menu bar
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-
-# Display date, day, and time in the menu bar
-defaults write com.apple.menuextra.clock DateFormat -string 'EEE d MMM HH:mm'
-
-
-# Increase keyboard initial delay
+echo "キーリピートの速度を速くする"
 defaults write -g InitialKeyRepeat -int 10
 
-# Increase keyboard repeat rate
+echo "キーリピートの待ち時間を短くする"
 defaults write -g KeyRepeat -int 1
 
-# Increase mouse speed
+echo "タップでクリックを許可"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -int 1
+
+echo "マウスの速度を速くする"
 defaults write -g com.apple.mouse.scaling 1.5
 
 # Use the Fn key as a standard function key
+echo "Fn キーを標準のファンクションキーとして使用"
 defaults write -g com.apple.keyboard.fnState -bool true
 
 # Increase trackpad speed
+echo "トラックパッドの速度を速くする"
 defaults write -g com.apple.trackpad.scaling 3
 
 # Show files with all extensions
+echo "全ての拡張子のファイルを表示"
 defaults write -g AppleShowAllExtensions -bool true
 
-# Dock に標準で入っている全てのアプリを消す（Finder とごみ箱は消えない）
+echo "Dock に標準で入っている全てのアプリを消す（Finder とごみ箱は消えない）"
 defaults write com.apple.dock persistent-apps -array
+
+echo "ナチュラルなスクロールを無効にする"
+defaults write -g com.apple.swipescrolldirection -bool false
 
 
 for app in "Dock" \
