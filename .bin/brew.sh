67000d1bbffecd1a683e54b99847b76b51c20b13
newt <ysti2019.win2020.st@gmail.com>
2025-02-12 13:05:19 +0900

    fix Brewfile path

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -6,6 +6,6 @@ if [ "$(uname)" != "Darwin" ] ; then
 fi
 
 BREWFILE_DIR="$(cd "$(dirname "$0")" && pwd)"
-brew bundle --file="$BREWFILE_DIR/Brewfile" --global
+brew bundle --file="./Brewfile" --global
 
-mas install 1429033973 # https://github.com/Kyome22/menubar_runcat/issues/1#issuecomment-778353093
+# TODO: RunCatのインストール
