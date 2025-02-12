999cbb75b9082c81ecf67c9b187ae8e5f92b8a92
newt <ysti2019.win2020.st@gmail.com>
2025-02-12 11:31:23 +0900

    update init.sh

--- .bin/init.sh
+++ .bin/init.sh
@@ -5,11 +5,8 @@ if [ "$(uname)" != "Darwin" ] ; then
 	exit 1
 fi
 
-# Set zsh
+echo "Set zsh"
 chsh -s /bin/zsh
 
-# Install Xcode command line tools
-xcode-select --install > /dev/null
-
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
+echo "Install Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
