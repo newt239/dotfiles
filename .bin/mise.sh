5913d019ceebbf7119d95b53476916307372fee0
newt <ysti2019.win2020.st@gmail.com>
2025-02-13 20:37:57 +0900

    feat: improve mise setup

--- .bin/mise.sh
+++ .bin/mise.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+source ~/.zshrc
+
 # .mise.tomlファイルの存在を確認
 if [ ! -f "./.mise.toml" ]; then
   echo ".mise.tomlファイルが見つかりません。"
   exit 1
 fi
 
-# miseコマンドを実行してインストール
-mise install
-
-# シンボリックリンクを作成
-ln -sf "$(pwd)/.mise.toml" "$HOME/.config/mise/config.toml"
+echo "miseでインストール中......"
+mise use -g node
+mise use -g bun
+mise use -g cmake
+mise use -g deno
+mise use -g ffmpeg
+mise use -g go
+mise use -g pnpm
+mise use -g rust
 
 # インストールの結果を表示
 if [ $? -eq 0 ]; then
